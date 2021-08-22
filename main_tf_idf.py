4efbf9ec4607926a21492a2f14456831ac535602
Andrei Vlasov <andrey.v.vlasov@phystech.edu>
2021-08-22 06:08:06 +0300

    Add files via upload
    добавлена финальная модель mT5

--- main_tf_idf.py
+++ main_tf_idf.py
@@ -8,20 +8,23 @@ import os
 from sklearn.feature_extraction.text import TfidfVectorizer
 import argparse
 
-parser = argparse.ArgumentParser(description='A tutorial of argparse!')
+parser = argparse.ArgumentParser(description='argparse')
 parser.add_argument("--i",  type=str, help="path to yout json file_name which we should read")
 parser.add_argument("--o",  type=str, help="path to yout json file_name in which we should write")
 parser.add_argument("--string_limit", default=30, type=int, help="limit len of predicted string")
 
+# список наиболее популярных стоп слов
 stop_words = pd.read_csv('stopwords.txt')
 stop_words = list(stop_words['c'])
-stop_words = stop_words + ['URL','USER','HASHTAG','ap','ар', '2ар', 'interfax', 'ru']
+stop_words = stop_words + ['URL','USER','HASHTAG','ap','ар', '2ар', 'interfax', 'ru','ak']
 
+# установка диапазона n-gram и колонки, которая используется для обучения модели
 best_params = {
     'gram_range':(1,5),
     'text2model':'input_text'
 }
 
+# функция убирает стоп слова
 def removeStopwords(wordlist, stopwords):
     return [w for w in (wordlist) if w not in stopwords]
 
@@ -29,6 +32,10 @@ translator = str.maketrans('', '', string.punctuation)
 
 
 def preprocess_text(Text,punct=False, figures=False, stopwords = False):
+    """
+    На вход подается колонка с текстом, текст идет в нижний регистр убираются знаки препинания и сторонние символы,
+    смайлы, хештеги, по команде убираются стоп слова и цифры
+    """
     result=[]
     for sentense in tqdm(Text):
         string=(sentense.lower())
@@ -65,6 +72,9 @@ def preprocess_text(Text,punct=False, figures=False, stopwords = False):
     return result
 
 def dataset_creation(data:list):
+    """
+    Происходит сборка датафрейма из json файла, т.к. обучение идет на заголовках, новости : молния автоматически становятся заголовками
+    """
     df_val = pd.DataFrame()
 
     test_count = []
@@ -95,6 +105,9 @@ def dataset_creation(data:list):
     return df_val
 
 def count_tokens(text):
+    """
+    Подсчет токенов
+    """
     text = text.split()
     cnt = 0
     for word in text:
@@ -105,7 +118,17 @@ def count_tokens(text):
 
 
 def check_max_tfidf(mega_title, df_val, target_title,col_print, gram_range = (1,3), limit_string=30):
-    
+    """
+    Расчет матрицы tf-idf
+
+    mega_title: сюжет
+    df_val: входной датафрейм
+    target_title: название поля с сюжетами
+    gram_range: range n-gram
+    limit_string: трешхолд на размер предсказания
+
+    Функция делает суммаризацию для 1 сюжета
+    """
     df_check = df_val[df_val[target_title] == mega_title]
 
     corpus = df_check[col_print].values
@@ -116,11 +139,9 @@ def check_max_tfidf(mega_title, df_val, target_title,col_print, gram_range = (1,
 
     max_values = df_tf_idf.max(axis=0)
 
-    # пока так далее лучше ранжировать в зависимости от нграмм и символов до 30  
     flag_2_next_max_value = True
     while flag_2_next_max_value:
         
-        # пока так далее лучше ранжировать в зависимости от нграмм и символов до 30  
         cols_w_max_values = max_values[max_values == np.max(max_values)]
 
         df_best = cols_w_max_values.to_frame().reset_index()
@@ -128,7 +149,6 @@ def check_max_tfidf(mega_title, df_val, target_title,col_print, gram_range = (1,
         df_best['count_name'] = df_best['best_name'].apply(lambda x: len(x))
         df_best['count_tokens'] = df_best['best_name'].apply(lambda x: count_tokens(x))
         
-        ## мб стоит ещё сортировать от максимального количества (мб минимального) символов
         df_best.sort_values(['count_tokens'], ascending=False, inplace=True)
 
         best_col = df_best['best_name'].iloc[0]
@@ -142,6 +162,9 @@ def check_max_tfidf(mega_title, df_val, target_title,col_print, gram_range = (1,
     return best_col, df_best
 
 def make_predictions_tfidf(dataset, target_title, limit_string):
+    """
+    Предсказание для списка сюжетов
+    """
     preds_baseline = []
     mega_titles = dataset[target_title].unique()
     for megatitle_num in tqdm(range(len(mega_titles))):
@@ -157,7 +180,16 @@ def make_predictions_tfidf(dataset, target_title, limit_string):
     return preds_baseline
 
 def main(file_name_in:str, string_limit:int, file_name_out:str,):
+    """
+    На вход подается название json файла, на выходе получаем список предсказанных сюжетов,
+    который записывается в текстовый файл.
 
+    Функция: - считывает файл
+             - приводит его в датафрейм
+             - производит препроцессинг
+             - делает предсказание на основе tf-idf
+             - производит запись в файл
+    """
     with open(file_name_in,'r') as file:
         data = json.load(file)
 
@@ -165,7 +197,7 @@ def main(file_name_in:str, string_limit:int, file_name_out:str,):
 
     dataset = dataset_creation(data)
     dataset.loc[dataset[dataset['type_id'] == 'FLASH'].index,'title'] = dataset[dataset['type_id'] == 'FLASH']['text']
-    dataset = dataset[['title','mega_title','text']]
+    dataset = dataset[['title','mega_title','text', 'num_story']]
     dataset = dataset.rename(columns={'title':'input_text','mega_title':'target_text','text':'text_all'})
     dataset['input_text'] = preprocess_text(dataset['input_text'], True, stopwords=True)
 
@@ -175,12 +207,9 @@ def main(file_name_in:str, string_limit:int, file_name_out:str,):
     # убираем инфу до интерфакс (заглушка)
     dataset['text_all'] = dataset['text_all'].apply(lambda x: x.split('интерфакс',1)[1] if 'интерфакс' in x else x)
 
-    # для МТ5
-    dataset['prefix'] = ['summarization']*len(dataset)
-    
     # делаем предсказание для tfidf
-    preds_baseline = make_predictions_tfidf(dataset,target_title='target_text',limit_string=string_limit)
-    print(preds_baseline)
+    preds_baseline = make_predictions_tfidf(dataset,target_title='num_story',limit_string=string_limit)
+    print(len(preds_baseline))
     with open(file_name_out, 'w+') as fout:
         for preds in preds_baseline: 
             fout.write(preds+'\n')
